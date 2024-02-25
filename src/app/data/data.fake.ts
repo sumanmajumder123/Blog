0cfc4c882746ed2718f453bcbdaadefd1327a402
Suman Majumder <sum4n_m@hotmail.com>
2024-02-25 08:39:52 -0300

    Upload do projeto

---
+++ src/app/data/data.fake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "https://s2-techtudo.glbimg.com/MJbvL9_XD1CTbaoJEc-nOOr5a3Q=/0x0:2048x1080/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2024/c/O/aO321qTQCdNQ1mw3H5hg/aaaaqc0tajz4oomzldpzu-dfsvex6dvucgrbv562fn-hr1wcqeuqq9-buuogb2mzr0mlcesmhd7kdzfauzhmuakegfgrxdhrzydkwx3xrihiz-qku75buil1lfrffxjabfrcke26-pjxrtmaejx-g7ptpvoqzvu.jpg",
+        "title": "Griselda é o novo sucesso da Netflix",
+        "description": "Conheça a história real de como Griselda dominou Miami"
+    },
+    {
+        "id": "2",
+        "photo": "https://cinepop.com.br/wp-content/uploads/2024/02/avatar-netflix-o-ultimo-mestre-do-ar.webp",
+        "title": "Lançamento: Último Meste do Ar",
+        "description": "Conheça a grande jornada de Aang e seus amigos."
+
+    },
+    {
+        "id": "3",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbqAhv4ZQi3Epl_tZQ9C4bCQSO235vSzld9Zijk4wxsw&s",
+        "title": "Venha se divertir com Machos Alfa",
+        "description": "O mundo dos machos não é mais o mesmo..."
+
+    },
+    {
+        "id": "4",
+        "photo": "https://d17lbu6bbzbdc8.cloudfront.net/wp-content/uploads/2020/07/09173006/series-netflix.jpg",
+        "title": "Outras séries que você pode gostar.",
+        "description": "Confira outras séries da netflix."
+
+    }
+]
